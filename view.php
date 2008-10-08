643eb5b7db7647bdde2f0a0561c9f46b9a177b4a
Albert Gasset <albert.gasset@gmail.com>
2008-10-08 11:19:53 +0200

    no permetre accedir quan la versió de codi i bd no crresponen

--- view.php
+++ view.php
@@ -2,9 +2,16 @@
 
 require_once '../../config.php';
 
+require_once 'version.php';
 require_once 'lib.php';
 require_once 'db.php';
 require_once 'url.php';
+
+$installed_version = get_field('modules', 'version', 'name', 'fct');
+if ($installed_version != $module->version) {
+    error("No es pot utilitzar temporalment el mòdul FCT "
+          . "perquè s'estan realitzant tasques de manteniment.");
+}
 
 $pagina = optional_param('pagina', 'llista_quaderns', PARAM_ALPHAEXT);
 include_once "pagines/$pagina.php";
