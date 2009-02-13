750bbc0a2065c7a3a6dee439d06989f23e7c816a
Albert Gasset <albert.gasset@gmail.com>
2009-02-13 11:54:57 +0100

    dades_relatives: millores hores pendents i ajuda

--- pagines/form_dades_relatives.php
+++ pagines/form_dades_relatives.php
@@ -5,22 +5,18 @@ fct_require('pagines/form_base.php');
 class fct_form_dades_relatives extends fct_form_base {
 
     function configurar() {
-        $this->afegir_header('dades_relatives', fct_string('dades_relatives'));
+        $this->afegir_header('dades_relatives', fct_string('dades_relatives'),
+                             'dades_relatives');
         $this->afegir_text('hores_credit', fct_string('hores_credit'), 6, false, true);
         $this->afegir_select('exempcio', fct_string('exempcio'),
             array(0 => '-', 25 => '25%', 50 => '50%'));
+        $this->afegir_text('hores_anteriors', fct_string('hores_anteriors'),
+                           6, false, true);
         if ($this->pagina->accio == 'veure') {
-            $this->afegir_static('hores_exempcio',fct_string('hores_exempcio'), '');
-            $this->afegir_static('hores_practiques',
-                                 fct_string('hores_practiques'), '', ':',
-                                 'dades_relatives_hores_practiques');
-        }
-        $this->afegir_text('hores_anteriors', fct_string('hores_anteriors'), 6,
-                           false, true, ':', 'dades_relatives_hores_anteriors');
-        if ($this->pagina->accio == 'veure') {
-            $this->afegir_static('hores_realitzades',
-                                 fct_string('hores_realitzades'), '', ':',
-                                 'dades_relatives_hores_realitzades');
+            $this->afegir_static('hores_realitzades_detall',
+                                 fct_string('hores_realitzades'),
+                                 fct_string('hores_realitzades_detall',
+                                            $this->pagina->dades));
             $this->afegir_static('hores_pendents', fct_string('hores_pendents'), '');
         }
 
