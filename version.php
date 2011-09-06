aea6bb6432fb293f0f4965c81af692757a1e5ebb
Albert Gasset <albert.gasset@gmail.com>
2011-09-06 13:48:34 +0200

    versió 2.1

--- version.php
+++ version.php
@@ -4,12 +4,12 @@
  * Definició de la versió del mòdul.
  *
  * @author Albert Gasset Romo
- * @version 2.0
+ * @version 2.1
  * @package fct
  **/
 
 // Versió del mòdul en format AAAAMMDDXX (XX és incremental)
-$module->version = 2010091301;
+$module->version = 2011090600;
 
 // Versió de Moodle requerida
 $module->requires = 2007101590.000;
