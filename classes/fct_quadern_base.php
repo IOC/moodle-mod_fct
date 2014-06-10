a2bee49ba555b73249fec0e726e794051a641b17
Marc Català <reskit@gmail.com>
2014-06-10 18:33:16 +0200

    Fixed warnings generated by params that don't exist

--- classes/fct_quadern_base.php
+++ classes/fct_quadern_base.php
@@ -1,4 +1,27 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Quadern base FCT class
+ *
+ * @package    mod
+ * @subpackage fct
+ * @copyright  2014 Institut Obert de Catalunya
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
 require_once('fct_base.php');
 require_once('fct_quadern_quinzena.php');
@@ -23,7 +46,7 @@ class fct_quadern_base extends fct_base {
     public $empresa;
     public $dades_alumne;
     public $convenis;
-    public $hores_credit = 0 ;
+    public $hores_credit = 0;
     public $hores_practiques = 0;
     public $hores_anteriors = 0;
     public $exempcio = 0;
@@ -123,7 +146,7 @@ class fct_quadern_base extends fct_base {
         }
 
         if (array_key_exists($name, array_flip($dataobjectkeys))) {
-             return $this->$dataobject->$name;
+             return (isset($this->$dataobject->$name) ? $this->$dataobject->$name : '');
         }
         return false;
     }
