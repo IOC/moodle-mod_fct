efd52e81a6ace085d320bb1ecd098e393bc80868
Marc Català <mcatala@itteria.cat>
2015-07-14 17:07:54 +0200

    Increased number of quaderns per page

--- view.php
+++ view.php
@@ -23,7 +23,7 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-define('PAGENUM', 10);
+define('PAGENUM', 20);
 define('PAGENUMAVIS', 20);
 
 require_once('../../config.php');
